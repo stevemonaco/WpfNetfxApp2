b0d2be967974ed4e3bd8210b2095a9f6d9c10d4e
Stephen Monaco <stevemonaco@hotmail.com>
2022-01-05 06:12:20 -0500

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TestViewModel _viewModel;
+
+        public MainWindow()
+        {
+            _viewModel = new TestViewModel()
+            {
+                Current = 5,
+                Max = 50
+            };
+
+            DataContext = _viewModel;
+
+            InitializeComponent();
+
+            Task.Run(() => _viewModel.Update());
+        }
+    }
+}
+
---
+++ WpfApp2/TestClass.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Application = System.Windows.Application;
+
+namespace WpfApp2
+{
+    public class TestViewModel : INotifyPropertyChanged
+    {
+        private int _max;
+        public int Max
+        {
+            get => _max;
+            set => SetField(ref _max, value);
+        }
+
+        private int _current;
+        public int Current
+        {
+            get => _current;
+            set => SetField(ref _current, value);
+        }
+
+        public void Update()
+        {
+            while (Current < Max)
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    Current++;
+                });
+
+                Thread.Sleep(500);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
